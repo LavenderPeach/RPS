4ee77f958cedccbd571611c7981c1a124a1a1159
Cade <134173296+LavenderPeach@users.noreply.github.com>
2023-06-27 16:19:17 -0400

    Added scorekeeping and instant button input.

--- rps.js
+++ rps.js
@@ -1,3 +1,33 @@
+let playerScore = 0
+let computerScore = 0
+const rockButton = document.querySelector('#rockBtn')
+const paperButton = document.querySelector('#paperBtn')
+const scissorsButton = document.querySelector('#scissorsBtn')
+const results = document.querySelector('.results')
+const playerScoreSpan = document.querySelector('.player-score')
+const computerScoreSpan = document.querySelector('.opponent-score')
+
+  function playRound(playerSelection, computerSelection) {
+    if (computerSelection == playerSelection) {
+      const p = document.createElement('p')
+      p.innerText = 'You Tied!';
+      results.appendChild(p)
+    } else if (
+        (computerSelection == 'Rock' && playerSelection == 'Scissors') ||
+        (computerSelection == 'Scissors' && playerSelection == 'Paper') ||
+        (computerSelection == 'Paper' && playerSelection == 'Rock') )
+        { const p = document.createElement('p')
+        p.innerText = 'You Lose!';
+        results.appendChild(p)
+        computerScore++
+    } else {
+        const p = document.createElement('p')
+        p.innerText = 'You Win!';
+        results.appendChild(p)
+        playerScore++
+    }
+}
+
 function getComputerChoice() {
     const randomNumber = (Math.floor(Math.random() * 3))
     if (randomNumber === 0){
@@ -7,21 +37,49 @@ function getComputerChoice() {
     } else if (randomNumber === 2){
     return 'Scissors';
     }
-      }
+  }
 
-  function playRound(playerSelection, computerSelection) {
-    if (computerSelection == playerSelection) {
-        return ('You Tied!');
-    } else if (
-        (computerSelection == 'Rock' && playerSelection == 'Scissors') ||
-        (computerSelection == 'Scissors' && playerSelection == 'Paper') ||
-        (computerSelection == 'Paper' && playerSelection == 'Rock') )
-        return ('You Lose!')
-     else {
-        return ('You Win!')
-    }
+const gameOver = (playerScore, computerScore) => {
+  if (playerScore === 5) {
+    const h2 = document.createElement('h2')
+    h2.classList.add('victory')
+    h2.innerText = 'WOOOOOOOOOOO :)'
+    results.append(h2)
+  }
+  if (computerScore === 5) {
+     const h2 = document.createElement('h2')
+     h2.classList.add('defeat')
+    h2.innerText = 'NOOOOOOOOO D:'
+    results.append(h2)
+  }
 }
 
-const playerSelection ="Rock"
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+const updateScores = (playerScore, computerScore) => {
+  playerScoreSpan.innerText = `Player Score: ${playerScore}`
+  computerScoreSpan.innerText = `Computer Score: ${computerScore}`
+}
+
+rockButton.addEventListener('click', () => {
+  const computerSelection = getComputerChoice()
+  const playerSelection = 'Rock'
+  playRound(playerSelection, computerSelection)
+  updateScores(playerScore, computerScore)
+  gameOver(playerScore, computerScore)
+});
+
+paperButton.addEventListener('click', () => {
+  const computerSelection = getComputerChoice()
+  const playerSelection = 'Paper'
+  playRound(playerSelection, computerSelection)
+  updateScores(playerScore, computerScore)
+  gameOver(playerScore, computerScore)
+});
+
+scissorsButton.addEventListener('click', () => {
+  const computerSelection = getComputerChoice()
+  const playerSelection = 'Scissors'
+  playRound(playerSelection, computerSelection)
+  updateScores(playerScore, computerScore)
+  gameOver(playerScore, computerScore)
+});
+
